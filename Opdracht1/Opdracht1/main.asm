cf447e2ba420d50d5a62a052f9011da165c25b7a
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2021-12-13 19:41:05 +0100

    Proefwerk
    Startprojecten

---
+++ Opdracht1/Opdracht1/main.asm
@@ -0,0 +1,10 @@
+; 6IT - Proefwerk TSO - DEEL1
+; Opdracht1.asm
+;
+; 14/12/2021
+;
+; naam: 
+;
+
+
+
---
+++ Opdracht2/Opdracht2/main.c
@@ -0,0 +1,19 @@
+/*6IT - Proefwerk TSO - DEEL1
+*Opdracht2
+*
+*14/12/2021
+*
+*naam:
+*
+*/ 
+
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
